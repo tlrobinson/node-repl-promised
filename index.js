315c9573c0690ea94129db4840cc0263bdf6083d
Pasindu Perera <udnisap@users.noreply.github.com>
2015-11-25 21:13:13 +0530

    Handled the auto completes on the repl.

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var Q = require("q");
 exports.promisify = function(repl) {
   var _eval = repl.eval;
   repl.eval = function(cmd, context, filename, callback) {
-    Q.nfcall(_eval, cmd, context, filename).then(function(result) {
-      callback(null, result);
-    }, function(err) {
-      callback(err);
+    Q.nfcall(_eval, cmd, context, filename).then(function() {
+        callback(null, arguments);
+    }, function() {
+        callback(arguments);
     });
   }
 }
