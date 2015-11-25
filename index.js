1c3453b3924085111186b775f5eae4ae3287a248
Pasindu Perera <udnisap@users.noreply.github.com>
2015-11-25 22:27:25 +0530

    pass arguments instead of parameters

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ var Q = require("q");
 exports.promisify = function(repl) {
   var _eval = repl.eval;
   repl.eval = function(cmd, context, filename, callback) {
-    Q.nfcall(_eval, cmd, context, filename).then(function() {
-        callback(null, arguments);
+    Q.nfapply(_eval, arguments).then(function() {
+        //Success callback starts with null
+        Array.prototype.unshift.call(arguments, null);
+        callback.apply(this, arguments);
     }, function() {
-        callback(arguments);
+        callback.apply(this, arguments);
     });
   }
 }
